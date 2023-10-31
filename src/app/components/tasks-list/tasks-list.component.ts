b23c2fcc83c75fa5237be999e7e7cff7bb688a1e
Aleksandr Harisov <bwb-a.harisov@fondespierre.com>
2023-10-31 14:45:43 +0100

    added check for overdue date

--- src/app/components/tasks-list/tasks-list.component.ts
+++ src/app/components/tasks-list/tasks-list.component.ts
@@ -12,7 +12,11 @@ export class TasksListComponent {
 
   tasks: Task[] = [];
 
+  // booleans for alerts state
   success: boolean = false;
+  taskComplete: boolean = false;
+  taskReopened: boolean = false;
+  hasDeadline: boolean = false;
 
   constructor(
 
@@ -38,6 +42,65 @@ export class TasksListComponent {
     this.success = true;
 
     // hide alert after 1s
-    setTimeout(() => this.success = false, 800);
+    setTimeout(() => this.success = false, 1000);
+  }
+
+  onToggleComplete(id: number) {
+
+    const task: Task | undefined = this.taskService.getById(id); 
+
+    if (task) {
+
+      if (task.done) {
+
+        // show alert
+        this.taskReopened = true;
+
+        // hide alert after 1s
+        setTimeout(() => this.taskReopened = false, 1000);
+
+        task.done = false;
+      } else {
+        
+        // show alert
+        this.taskComplete = true;
+
+        // hide alert after 1s
+        setTimeout(() => this.taskComplete = false, 1000);
+
+        task.done = true;
+      }
+    }
+    
+  }
+
+  checkDate(date: string, done: any): string | undefined {
+
+    // transform task date to timestamp
+    const timestamp = Date.parse(date);
+    // get time now
+    const now = Date.now();
+    
+    // create date from now and add 3 days to it
+    const newDate = new Date(now);
+    newDate.setDate(newDate.getDate() + 3);
+
+    // check if deadline passed and task did not closed
+    if (now >= timestamp && !done) {
+      
+      // return class name for coloring table row
+      return 'deadline';
+
+    } else if (newDate.getTime() > timestamp && !done) { // check if task date is less then time now + 3 days
+      
+      // return class name for coloring table row
+      return 'warn';
+
+    } else {
+
+      return '';
+    }
+
   }
 }
+
